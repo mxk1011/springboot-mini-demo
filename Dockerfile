646d3f42ae37e7c8ab228fd20813eb5a311c4e7e
Maximilian Schmidt <schmidtmaximilian@online.de>
2020-06-20 16:36:08 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:18.04
+
+EXPOSE 443
+
+RUN apt-get update
+RUN apt-get install curl -y
+RUN apt install apt-utils -y
+RUN apt install openjdk-11-jdk openjdk-11-jre -y
+RUN apt-get install software-properties-common -y
+
+RUN mkdir /var/www
+WORKDIR /var/www
+RUN cd /var/www
+
+COPY . .
+
+ENTRYPOINT mvn -U clean spring-boot:run
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    @CrossOrigin(origins = "*")
+    public String getOne() {
+        return "I am alive!";
+    }
+}
