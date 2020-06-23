890d31109a2e3ff01f526e892c4aa2c05f59102a
Maximilian Schmidt <schmidtmaximilian@online.de>
2020-06-23 17:00:55 +0200

    Testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+		return (factory) -> {
+			((TomcatServletWebServerFactory) factory)
+					.addConnectorCustomizers((connector) -> {
+						((AbstractHttp11Protocol<?>) connector.getProtocolHandler())
+								.setUseServerCipherSuitesOrder(true);
+					});
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
